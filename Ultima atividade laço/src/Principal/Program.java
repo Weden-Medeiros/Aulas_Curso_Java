be837e7d9bba038e1142104e4f8060b363543583
Weden <Wedenbsb@gmail.com>
2023-05-23 21:07:57 -0300

    Ultima atividade laço

--- Ultima atividade laço/src/Principal/Program.java
+++ Ultima atividade laço/src/Principal/Program.java
@@ -6,34 +6,33 @@ public class Program
 public static void main(String[] args) 
 			{
 		  
-		        Scanner sc = new Scanner(System.in);
-		        System.out.print("Digite o tipo de operação (soma, divisão, multiplicação, subtração): ");
-		        String operacao = sc.nextLine();
-		        System.out.print("Digite o número: ");
-		        int num = sc.nextInt();
-		        
-		        for (int i = 1; i <= 10; i++)
-		        {
-		            if (operacao.equals("soma"))
-		            	{
-		                	System.out.println(num + " + " + i + " = " + (num + i));
-		            	} 	
-		            else if (operacao.equals("divisão")) 
-		            	{
-		            		System.out.println(num + " / " + i + " = " + ((double) num / i));
-		                
-		            	} 	
-		            else if (operacao.equals("multiplicação")) 
-		            	{
-		                	System.out.println(num + " * " + i + " = " + (num * i));
-		            	} 			
-		            else if (operacao.equals("subtração"))
-		            	
-		            	{
-		            		System.out.println(num + " - " + i + " = " + (num - i));
-		            	}
-		        }
-		        sc.close();
-		    }
+           
+		                 Scanner sc = new Scanner(System.in);
+		                 System.out.println("Digite a operação (soma, subtração, multiplicação, divisão): ");
+		                 String operacao = sc.nextLine();
+		                 System.out.println("Digite o número: ");
+		                 int num = sc.nextInt();
+		                 for (int i = 1; i <= 10; i++) 
+		                 {
+		                 
+		                  if (operacao.equals("soma")) 
+		                  	{
+		                      System.out.println(num + " + " + i + " = " + (num + i));
+		                  	}
+		                  else if (operacao.equals("subtração")) 
+		                  	{
+		                	  System.out.println((num + i) + " - " + num + " = " + i);
+		                    }
+		                  else if (operacao.equals("multiplicação")) 
+		                     {
+		                      System.out.println(num + " * " + i + " = " + (num * i));
+		                     } 
+		                  else if (operacao.equals("divisão"))
+		                     {
+		                	  System.out.println((num * i) + " / " + num + " = " + i);	                                       
+		                     }		                                                     
+		                 }	                            	        
+		       sc.close();
+		   }
 	
 }
